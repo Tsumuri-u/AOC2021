f7c56b3fed66368a7580abec8415977cfdaab81e
Ariyan Afrin <ariyanafrin@protonmail.com>
2021-12-07 18:15:45 -0800

    begin day 2

---
+++ Day 02: Dive!/day02.py
@@ -0,0 +1,16 @@
+fileObject = open("input02.txt", "r")
+data = fileObject.read()
+data = data.split()
+
+def part_one():
+    horizontalPosition = 0
+    verticalPosition = 0
+    answer = 0
+    return
+    
+part_one()
+
+print(data[0])
+print(data[1])
+print(data[2])
+print(data[3])
