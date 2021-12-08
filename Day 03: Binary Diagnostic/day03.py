db66d731f2bbb26041979e141fd3972687486be1
Ariyan Afrin <ariyanafrin@protonmail.com>
2021-12-07 21:56:36 -0800

    Finish day 2 part 1

---
+++ Day 03: Binary Diagnostic/day03.py
@@ -0,0 +1,38 @@
+fileObject = open("input03.txt", "r")
+data = fileObject.read()
+
+def part_one():
+    gammaRate = ""
+    epsilonRate = ""
+    answer = 0
+    
+    for i in range(12):
+        zeroCount = 0
+        oneCount = 0
+        
+        j = i
+        while j < len(data):
+            if int(data[j]) == 0:
+                zeroCount+=1
+            elif int(data[j]) == 1:
+                oneCount+=1
+            j += 13
+        
+        if zeroCount >= oneCount:
+            gammaRate += "0"
+        elif oneCount > zeroCount:
+            gammaRate += "1"
+    
+    for i in range(len(gammaRate)):
+        if gammaRate[i] == "0":
+            epsilonRate += "1"
+        elif gammaRate[i] == "1":
+            epsilonRate += "0"
+            
+    answer = (int(gammaRate, 2)) * (int(epsilonRate, 2))
+    print("Part 1: " + str(answer))
+    
+    
+    
+part_one()
+
